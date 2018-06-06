5bedd4d59a6b2dff5035994c0e6cc0ddc66c845d
HDB-Li <358149116@qq.com>
2018-06-06 14:48:58 +0800

    Add statusbar in screenshot.
    * Add statusbar in screenshot

--- LLDebugTool/Helper/ScreenShotHelper/LLScreenshotHelper.m
+++ LLDebugTool/Helper/ScreenShotHelper/LLScreenshotHelper.m
@@ -89,34 +89,42 @@ static LLScreenshotHelper *_instance = nil;
     
     UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
     CGContextRef context = UIGraphicsGetCurrentContext();
-    for (UIWindow *window in [[UIApplication sharedApplication] windows])
+    
+    NSMutableArray *windows = [[NSMutableArray alloc] initWithArray:[[UIApplication sharedApplication] windows]];
+    UIView *statusBar = [[UIApplication sharedApplication] valueForKey:@"_statusBar"];
+    if ([statusBar isKindOfClass:[UIView class]]) {
+        [windows addObject:statusBar];
+    }
+    for (UIView *window in windows)
     {
-        CGContextSaveGState(context);
-        CGContextTranslateCTM(context, window.center.x, window.center.y);
-        CGContextConcatCTM(context, window.transform);
-        CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
-        if (orientation == UIInterfaceOrientationLandscapeLeft)
-        {
-            CGContextRotateCTM(context, M_PI_2);
-            CGContextTranslateCTM(context, 0, -imageSize.width);
+        if (!window.isHidden) {
+            CGContextSaveGState(context);
+            CGContextTranslateCTM(context, window.center.x, window.center.y);
+            CGContextConcatCTM(context, window.transform);
+            CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+            if (orientation == UIInterfaceOrientationLandscapeLeft)
+            {
+                CGContextRotateCTM(context, M_PI_2);
+                CGContextTranslateCTM(context, 0, -imageSize.width);
+            }
+            else if (orientation == UIInterfaceOrientationLandscapeRight)
+            {
+                CGContextRotateCTM(context, -M_PI_2);
+                CGContextTranslateCTM(context, -imageSize.height, 0);
+            } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+                CGContextRotateCTM(context, M_PI);
+                CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+            }
+            if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+            {
+                [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+            }
+            else
+            {
+                [window.layer renderInContext:context];
+            }
+            CGContextRestoreGState(context);
         }
-        else if (orientation == UIInterfaceOrientationLandscapeRight)
-        {
-            CGContextRotateCTM(context, -M_PI_2);
-            CGContextTranslateCTM(context, -imageSize.height, 0);
-        } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
-            CGContextRotateCTM(context, M_PI);
-            CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
-        }
-        if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
-        {
-            [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
-        }
-        else
-        {
-            [window.layer renderInContext:context];
-        }
-        CGContextRestoreGState(context);
     }
     
     UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
