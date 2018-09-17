70f331c4b68f82d99745971df367201eabe1c8e3
HDB-Li <358149116@qq.com>
2018-09-17 11:08:36 +0800

    Add a interface.

--- LLDebugTool/Helper/ScreenShotHelper/LLScreenshotHelper.h
+++ LLDebugTool/Helper/ScreenShotHelper/LLScreenshotHelper.h
@@ -22,6 +22,7 @@
 //  SOFTWARE.
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface LLScreenshotHelper : NSObject
 
@@ -45,6 +46,6 @@
 /**
  Return a image from current screen.
  */
-- (nullable UIImage *)imageFromScreen;
+- (UIImage *_Nullable)imageFromScreen;
 
 @end
