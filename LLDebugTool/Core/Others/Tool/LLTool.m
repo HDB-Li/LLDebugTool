dbaed6d8ecfd689e540c337bc780374b9f15ad63
HDB-Li <358149116@qq.com>
2019-08-26 18:35:06 +0800

    Remove unused files.

--- LLDebugTool/Core/Others/Tool/LLTool.m
+++ LLDebugTool/Core/Others/Tool/LLTool.m
@@ -24,16 +24,8 @@
 #import "LLTool.h"
 #import "LLConfig.h"
 #import "LLMacros.h"
-#import "LLFactory.h"
-#import "UIView+LL_Utils.h"
 #import "LLFormatterTool.h"
 #import "LLDebugTool.h"
-
-static CGFloat _toastTime = 2.0;
-
-static UILabel *_toastLabel = nil;
-static UILabel *_loadingLabel = nil;
-static NSTimer *_loadingTimer = nil;
 
 static unsigned long long _absolutelyIdentity = 0;
 
@@ -78,103 +70,6 @@ static unsigned long long _absolutelyIdentity = 0;
         height = gap;
     }
     return CGRectMake(x, y, width, height);
-}
-
-+ (void)toastMessage:(NSString *)message {
-    if (_toastLabel) {
-        [_toastLabel removeFromSuperview];
-        _toastLabel = nil;
-    }
-    
-    __block UILabel *label = [LLFactory getLabel:[UIApplication sharedApplication].delegate.window frame:CGRectMake(20, 0, LL_SCREEN_WIDTH - 40, 100) text:message font:17 textColor:[UIColor whiteColor]];
-    label.numberOfLines = 0;
-    label.lineBreakMode = NSLineBreakByCharWrapping;
-    label.textAlignment = NSTextAlignmentCenter;
-    label.alpha = 0;
-    label.backgroundColor = [UIColor blackColor];
-    label.LL_horizontalPadding = 10;
-    label.LL_verticalPadding = 5;
-    [label sizeToFit];
-    
-    [label LL_setCornerRadius:5];
-    label.center = CGPointMake(LL_SCREEN_WIDTH / 2.0, LL_SCREEN_HEIGHT / 2.0);
-    _toastLabel = label;
-    [UIView animateWithDuration:0.25 animations:^{
-        label.alpha = 1;
-    } completion:^(BOOL finished) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_toastTime * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            [UIView animateWithDuration:0.1 animations:^{
-                _toastLabel.alpha = 0;
-            } completion:^(BOOL finished) {
-                [_toastLabel removeFromSuperview];
-                _toastLabel = nil;
-            }];
-        });
-    }];
-}
-
-+ (void)loadingMessage:(NSString *)message {
-    if (_loadingLabel) {
-        [_loadingLabel removeFromSuperview];
-        _loadingLabel = nil;
-    }
-    
-    __block UILabel *label = [LLFactory getLabel:[UIApplication sharedApplication].delegate.window frame:CGRectMake(20, 0, LL_SCREEN_WIDTH - 40, 100) text:message font:17 textColor:[UIColor whiteColor]];
-    label.textAlignment = NSTextAlignmentCenter;
-    label.numberOfLines = 0;
-    label.LL_horizontalPadding = 10;
-    label.LL_verticalPadding = 5;
-    [label sizeToFit];
-    
-    [label LL_setCornerRadius:5];
-    label.center = CGPointMake(LL_SCREEN_WIDTH / 2.0, LL_SCREEN_HEIGHT / 2.0);
-    label.alpha = 0;
-    label.backgroundColor = [UIColor blackColor];
-    label.textColor = [UIColor whiteColor];
-    [[UIApplication sharedApplication].delegate.window addSubview:label];
-    _loadingLabel = label;
-    [UIView animateWithDuration:0.25 animations:^{
-        label.alpha = 1;
-    } completion:^(BOOL finished) {
-        
-    }];
-    [self startLoadingMessageTimer];
-}
-
-+ (void)hideLoadingMessage {
-    if (_loadingLabel.superview) {
-        [self removeLoadingMessageTimer];
-        [UIView animateWithDuration:0.1 animations:^{
-            _loadingLabel.alpha = 0;
-        } completion:^(BOOL finished) {
-            [_loadingLabel removeFromSuperview];
-            _loadingLabel = nil;
-        }];
-    }
-}
-
-+ (void)startLoadingMessageTimer {
-    [self removeLoadingMessageTimer];
-    _loadingTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(loadingMessageTimerAction:) userInfo:nil repeats:YES];
-}
-
-+ (void)removeLoadingMessageTimer {
-    if ([_loadingTimer isValid]) {
-        [_loadingTimer invalidate];
-        _loadingTimer = nil;
-    }
-}
-
-+ (void)loadingMessageTimerAction:(NSTimer *)timer {
-    if (_loadingLabel.superview) {
-        if ([_loadingLabel.text hasSuffix:@"..."]) {
-            _loadingLabel.text = [_loadingLabel.text substringToIndex:_loadingLabel.text.length - 3];
-        } else {
-            _loadingLabel.text = [_loadingLabel.text stringByAppendingString:@"."];
-        }
-    } else {
-        [self removeLoadingMessageTimer];
-    }
 }
 
 + (NSString *)stringFromFrame:(CGRect)frame {
