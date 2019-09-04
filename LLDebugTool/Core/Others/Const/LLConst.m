e120e4e62522b945779b331687c586dc9fe54f94
HDB-Li <358149116@qq.com>
2019-09-04 15:02:59 +0800

    Update LLConst.m

--- LLDebugTool/Core/Others/Const/LLConst.m
+++ LLDebugTool/Core/Others/Const/LLConst.m
@@ -25,7 +25,7 @@
 
 CGFloat const kLLEntryWindowBallWidth = 50;
 CGFloat const kLLEntryWindowMinBallWidth = 30;
-CGFloat const kLLEntryWindowDisplayPercent = 0.3;
+CGFloat const kLLEntryWindowDisplayPercent = 0.5;
 CGFloat const kLLEntryWindowFirstDisplayPositionX = 0;
 CGFloat const kLLEntryWindowFirstDisplayPositionY = 200;
 CGFloat const kLLEntryWindowInactiveAlpha = 0.75;
