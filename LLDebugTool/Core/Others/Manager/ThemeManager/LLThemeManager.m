c4fac002e802cd455536d8b3fc3e2703634be60e
HDB-Li <358149116@qq.com>
2019-09-05 17:49:53 +0800

    Update notification code line.

--- LLDebugTool/Core/Others/Manager/ThemeManager/LLThemeManager.m
+++ LLDebugTool/Core/Others/Manager/ThemeManager/LLThemeManager.m
@@ -50,16 +50,16 @@ NSString *const kThemeManagerUpdateBackgroundColorNotificaionName = @"kThemeMana
 - (void)setPrimaryColor:(UIColor * _Nonnull)primaryColor {
     if (_primaryColor != primaryColor) {
         _primaryColor = primaryColor;
-        [[NSNotificationCenter defaultCenter] postNotificationName:kThemeManagerUpdatePrimaryColorNotificaionName object:primaryColor];
         [self calculateColorIfNeeded];
+        [[NSNotificationCenter defaultCenter] postNotificationName:kThemeManagerUpdatePrimaryColorNotificaionName object:primaryColor];
     }
 }
 
 - (void)setBackgroundColor:(UIColor * _Nonnull)backgroundColor {
     if (_backgroundColor != backgroundColor) {
         _backgroundColor = backgroundColor;
-        [[NSNotificationCenter defaultCenter] postNotificationName:kThemeManagerUpdateBackgroundColorNotificaionName object:backgroundColor];
         [self calculateColorIfNeeded];
+        [[NSNotificationCenter defaultCenter] postNotificationName:kThemeManagerUpdateBackgroundColorNotificaionName object:backgroundColor];
     }
 }
 
