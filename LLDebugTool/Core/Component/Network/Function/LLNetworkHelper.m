69f80a85df1040ecc891cba8428aceeab4540d8f
HDB-Li <358149116@qq.com>
2019-09-27 14:58:34 +0800

    Update LLNetworkHelper.m

--- LLDebugTool/Core/Component/Network/Function/LLNetworkHelper.m
+++ LLDebugTool/Core/Component/Network/Function/LLNetworkHelper.m
@@ -57,11 +57,11 @@ static LLNetworkHelper *_instance = nil;
 }
 
 - (LLNetworkStatus)currentNetworkStatus {
-    if (@available(iOS 13.0, *)) {
-        return [self.reachability currentReachabilityStatus];
-    } else {
+//    if (@available(iOS 13.0, *)) {
+//        return [self.reachability currentReachabilityStatus];
+//    } else {
         return [self networkStateFromStatebar];
-    }
+//    }
 }
 
 #pragma mark - Primary
