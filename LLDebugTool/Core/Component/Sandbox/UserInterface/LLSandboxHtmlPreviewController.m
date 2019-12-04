dd3ffdfbc3f2026e5e879b6b897fc7d76619be42
HDB-Li <llworkinggroup@gmail.com>
2019-12-04 23:27:50 +0800

    Preview plist file.

--- LLDebugTool/Core/Component/Sandbox/UserInterface/LLSandboxHtmlPreviewController.m
+++ LLDebugTool/Core/Component/Sandbox/UserInterface/LLSandboxHtmlPreviewController.m
@@ -29,6 +29,8 @@
 #import "LLTool.h"
 
 #import "UIViewController+LL_Utils.h"
+#import "NSDictionary+LL_Utils.h"
+#import "NSArray+LL_Utils.h"
 
 @interface LLSandboxHtmlPreviewController () <WKNavigationDelegate, WKUIDelegate>
 
@@ -106,17 +108,37 @@
     
     self.title = [self.filePath lastPathComponent];
     
-    if (@available(iOS 9.0, *)) {
-        [self.webView loadFileURL:url allowingReadAccessToURL:url];
-    } else {
-        NSError *error = nil;
-        NSString *string = [[NSString alloc] initWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
-        if (error) {
-            [LLTool log:@"Load html string failed"];
+    if ([self.filePath.pathExtension caseInsensitiveCompare:@"plist"] == NSOrderedSame) {
+        NSDictionary *dic = [[NSDictionary alloc] initWithContentsOfURL:url];
+        NSArray *array = [[NSArray alloc] initWithContentsOfURL:url];
+        NSString *htmlString = nil;
+        if (dic != nil) {
+            htmlString = [dic LL_toJsonString];
+        } else if (array != nil) {
+            htmlString = [array LL_toJsonString];
+        }
+        if (!htmlString) {
+            [LLTool log:@"Load plist failed"];
             return;
         }
-        [self.webView loadHTMLString:string baseURL:[NSURL URLWithString:self.filePath.stringByDeletingLastPathComponent]];
+        [self loadHtmlString:htmlString];
+    } else {
+        if (@available(iOS 9.0, *)) {
+            [self.webView loadFileURL:url allowingReadAccessToURL:url];
+        } else {
+            NSError *error = nil;
+            NSString *string = [[NSString alloc] initWithContentsOfURL:url encoding:NSUTF8StringEncoding error:&error];
+            if (error) {
+                [LLTool log:@"Load html string failed"];
+                return;
+            }
+            [self loadHtmlString:string];
+        }
     }
+}
+
+- (void)loadHtmlString:(NSString *)string {
+    [self.webView loadHTMLString:string baseURL:[NSURL URLWithString:self.filePath.stringByDeletingLastPathComponent]];
 }
 
 #pragma mark - Getters and setters
