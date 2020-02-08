9404695c12ac245d01401b2754e65eae171b9d41
HDB-Li <llworkinggroup@gmail.com>
2020-02-08 22:01:57 +0800

    Fix.

--- LLDebugTool/Core/Component/Location/Function/MKMapView/MKMapView+LL_Location.m
+++ LLDebugTool/Core/Component/Location/Function/MKMapView/MKMapView+LL_Location.m
@@ -1,6 +1,8 @@
 //
 //  MKMapView+LL_Location.m
 //
+//  Copyright (c) 2018 LLDebugTool Software Foundation (https://github.com/HDB-Li/LLDebugTool)
+//
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
